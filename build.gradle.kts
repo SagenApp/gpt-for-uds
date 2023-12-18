dc856a11d2b4f275d888573321ca54402df823f7
Alexander Sagen <alexander.sagen@tripletex.no>
2023-12-19 00:41:07 +0100

    Update graalvm config shit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("org.graalvm.buildtools.native") version "0.9.9"
+    id("org.graalvm.buildtools.native") version "0.9.28"
 }
 
 group = "app.sagen"
@@ -20,25 +20,22 @@ java {
     }
 }
 
-nativeBuild {
-    imageName.set("my-native-app")
-}
-
 graalvmNative {
     binaries {
-        named("main").configure {
-            // Specific options for native-image can be set here
-            buildArgs.addAll(listOf(
-                "--no-fallback",
-                "--no-server",
-                "-O3",
-                "-H:Name=my-native-app", // Desired name of the native executable
-                "-H:Class=app.sagen.chatgptclient.UnixSocketServer", // Fully qualified name of your main class
-                "-H:ReflectionConfigurationFiles=${projectDir.toPath().resolve("reflection-config.json")}", // Path to your reflection config file
-                "-Duser.country=US",
-                "-Duser.language=en",
-                "-H:IncludeLocales=en"
-            ))
+        named("main") {
+            imageName.set("gpt-for-uds")
+            mainClass.set("app.sagen.chatgptclient.UnixSocketServer")
+            buildArgs.add("-O4")
+            buildArgs.add("--no-fallback")
+            buildArgs.add("--no-server")
+            buildArgs.add("-H:ReflectionConfigurationFiles=${projectDir.toPath().resolve("reflection-config.json")}")
+            debug.set(true)
+            verbose.set(true)
+            richOutput.set(true)
+
+            javaLauncher.set(javaToolchains.launcherFor {
+                languageVersion.set(JavaLanguageVersion.of(17))
+            })
         }
     }
 }
